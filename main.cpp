80baa011a6133ce60d6bb070b47501c79a3d950b
mOgOm <c0_0b@mail.ru>
2020-01-05 16:41:35 +0300

    Replace marker with counter in sPtr

--- main.cpp
+++ main.cpp
@@ -5,15 +5,30 @@
 #include <SOIL2/SOIL2.h>
 #include <exception>
 #include <iostream>
-
-
 #include "win.hpp"
+
+struct BinTree : Serializable {
+   sPtr l, r;
+   BinTree(Serializable *l_, Serializable *r_) :l(l_), r(r_) {}
+   sFields
+   __(l)
+   __(r)
+   sEnd
+};
+
 
 int main()
 {
    try {
-      Win wnd = {"window_settings.txt"};
-      wnd.run();
+      std::vector<BinTree> n;
+      n.push_back({nullptr, nullptr});
+      n.push_back({nullptr, nullptr});
+      n.push_back({&n[n.size() - 1], &n[n.size() - 2]});
+      std::ofstream file;
+      n.back() >> file;
+
+      //Win wnd = {"window_settings.txt"};
+      //wnd.run();
 
    } catch (std::exception const &err) {
       std::cout << err.what() << std::endl;
