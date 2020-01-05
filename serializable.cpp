478d408cf4c87733084447b323b9a9582694e92f
mOgOm <c0_0b@mail.ru>
2020-01-05 04:36:04 +0300

    Add serializable pointers that works with one simple struct

--- serializable.cpp
+++ serializable.cpp
@@ -1,4 +1,5 @@
 #include "serializable.hpp"
 std::string Serializable::depth{ "" };
+Serializable* Serializable::root_obj{ nullptr };
 //std::set<Serializable*> Serializable::ptrs;
 //std::set<Serializable*> Serializable::shown;
