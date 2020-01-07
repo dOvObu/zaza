c79af9090bb0b41e9fa529182135fe41ca567d36
mOgOm <c0_0b@main.ru>
2020-01-08 01:54:19 +0300

    Make tool for comfortable serialization of a tree

--- serializable.cpp
+++ serializable.cpp
@@ -1,5 +1,6 @@
 #include "serializable.hpp"
-std::string Serializable::depth{ "" };
-Serializable* Serializable::root_obj{ nullptr };
+std::string Serializable::depth = {};
+std::map<void*, std::vector<std::shared_ptr<sVec>>> Serializable::sVec_pool = {};
+Serializable* Serializable::root_obj = { nullptr };
 //std::set<Serializable*> Serializable::ptrs;
 //std::set<Serializable*> Serializable::shown;
